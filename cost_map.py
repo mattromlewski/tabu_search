b058b36345e0a56e5ba99d957445910944a4e1bf
mattromlewski <maromlew@edu.uwaterloo.ca>
2020-11-17 12:06:20 -0500

    [feat] QAP cost calculation

--- cost_map.py
+++ cost_map.py
@@ -14,18 +14,11 @@ class CostMap:
             reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC) # change contents to floats
             for row in reader: # each row is a list
                 self._cost_matrix.append(row)
-        
-        # build dictionary for fast cost lookup by a tuple of the objects
-        self._cost_dict = {}
-        i = 0
-        for row in self._cost_matrix:
-            #print(row)
-            for j in range(0, i):
-                self._cost_dict[(i,j)] = row[j]
-            i += 1
 
-    def get_cost_dict(self):
-        return self._cost_dict
+    def get_cost_matrix(self):
+        return self._cost_matrix
+
+    
 
 if __name__ == "__main__":
     map = CostMap("data/Flow.csv")
--- tabu_agent.py
+++ tabu_agent.py
@@ -1,3 +1,4 @@
+import numpy as np
 from typing import List
 
 from cost_map import CostMap
@@ -14,11 +15,36 @@ class TabuAgent:
         self._tabu_tenure = tabu_tenure
         self._max_iterations = max_iterations
         self._best_solution_value = 0
-        
         self._flows = CostMap("data/Flow.csv")
-        self._distances = CostMap("data/Distance.csv")
+        self._distances = np.array(CostMap("data/Distance.csv").get_cost_matrix())
 
-    def calculate_cost
+    def mutate_cost_map(self, neighbour_idx):
+        '''
+        Copies the current cost map, swaps two rows and two colums, and returns the modified array
+        Useful for both creating temporary flow matrices and for updating the master flow matrix
+        '''
+        mut_cost_map = [row[:] for row in self._flows.get_cost_matrix()]
+        # need to determine which row each of the swapped components occupy in the solution matrix
+        component_a, component_b = self._neighbour_swapped_components[neighbour_idx]
+        print("pair: {}   component_a: {}".format(self._neighbour_swapped_components[neighbour_idx], component_a))
+        component_a_idx = self._solution.index(component_a)  # This is the cost row and column occupied by component_a
+        component_b_idx = self._solution.index(component_b)
+        # Need to swap rows at the two indices above
+        temp = mut_cost_map[component_a_idx]
+        mut_cost_map[component_a_idx] = mut_cost_map[component_b_idx]
+        mut_cost_map[component_b_idx] = temp
+        # Now columns
+        for i in range(0, len(mut_cost_map)):
+            temp = mut_cost_map[i][component_a_idx]
+            mut_cost_map[i][component_a_idx] = mut_cost_map[i][component_b_idx]
+            mut_cost_map[i][component_b_idx] = temp
+        return mut_cost_map
+
+    def calculate_cost(self, neighbour_idx):
+        swapped = self._neighbour_swapped_components[neighbour_idx]
+        neighbour_cost_map = np.array(self.mutate_cost_map(neighbour_idx))
+        cost = np.sum(np.multiply(neighbour_cost_map, self._distances))/2.0
+        print(cost)
 
     def mutate_solution(self, position_1, position_2) -> List[int]:
         neighbour = list(self._solution)
@@ -28,8 +54,16 @@ class TabuAgent:
 
     def generate_neighbours(self):
         self._neighbours = []
+        solution_idx = 0
         for i in range(0, len(self._solution)-1):
-            for j in range(i+1):
+            for j in range(0,i):
                 self._neighbours.append(self.mutate_solution(i, j))
+                self._neighbour_swapped_components.append((i,j))
+                self._neighbour_values.append(self.calculate_cost(solution_idx))
+                solution_idx += 1
 
-
+if __name__ == "__main__":
+    agent = TabuAgent([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],10,1000)
+    init_cost = np.sum(np.multiply(np.array(agent._flows.get_cost_matrix()), agent._distances))/2.0
+    print("initial cost ={}".format(init_cost))
+    agent.generate_neighbours()
